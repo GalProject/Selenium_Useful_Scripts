4b0c9aa3c1d75ca5c0fc703b9be46798dfd77efd
GalBenEvgi <galevgi@gmail.com>
2017-03-04 10:48:06 +0200

    link added

--- BookDownload.py
+++ BookDownload.py
@@ -1,8 +1,12 @@
 from selenium import webdriver
 import urllib
 import time
+#Link: http://olvreader.sefereshet.org.il/Olive/OTB/OpenU/?href=C20242/2008/01/02&usticket=eW9zaXNoMw==&ticket=db2e5dc329bcbe119c052a86d8f97e8c2ca62ca6_yosish3
+
 
 driver = webdriver.Chrome("C:\\Users\\GalBenEvgi\\Desktop\\chromedriver.exe")
+
+
 
 for x in range(1,229):
     temp = x.__str__()
