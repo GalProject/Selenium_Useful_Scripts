417a7d1a87fe563306b29cb059c5b79f5b5f576e
GalBenEvgi <galevgi@gmail.com>
2018-01-03 21:12:05 +0200

    Link were added

---
+++ DownloadVideos.py
@@ -0,0 +1,8 @@
+from selenium import webdriver
+import urllib
+import time
+
+linkSrc = "http://stat.bgu.ac.il/vod/safotform01.mp4"
+
+
+driver = webdriver.Chrome("C:\\Users\\GalBenEvgi\\Desktop\\chromedriver.exe")
