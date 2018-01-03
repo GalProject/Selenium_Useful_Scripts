9dd321e62e1bedc07680baa44d1e9fea5f83f95d
GalBenEvgi <galevgi@gmail.com>
2018-01-03 21:16:13 +0200

    Link were added

--- DownloadVideos.py
+++ DownloadVideos.py
@@ -3,6 +3,11 @@ import urllib
 import time
 
 linkSrc = "http://stat.bgu.ac.il/vod/safotform01.mp4"
+# driver = webdriver.Chrome("C:\\Users\\GalBenEvgi\\Desktop\\chromedriver.exe")
 
+for x in range(1, 24):
+    if x < 10:
+        print '0' + str(x)
+    else:
+        print str(x)
 
-driver = webdriver.Chrome("C:\\Users\\GalBenEvgi\\Desktop\\chromedriver.exe")
