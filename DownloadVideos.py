83aa47bb1dea2a34e76b7515b19f3622a293b3ba
GalBenEvgi <galevgi@gmail.com>
2018-01-03 21:51:28 +0200

    progress bar were added

--- DownloadVideos.py
+++ DownloadVideos.py
@@ -1,13 +1,22 @@
 from selenium import webdriver
 import urllib
 import time
+import requests
 
-linkSrc = "http://stat.bgu.ac.il/vod/safotform01.mp4"
-# driver = webdriver.Chrome("C:\\Users\\GalBenEvgi\\Desktop\\chromedriver.exe")
+link = "http://stat.bgu.ac.il/vod/safotform01.mp4"
+file_name = "safot01again.mp4"
+with open(file_name, "wb") as f:
+        print "Downloading %s" % file_name
+        response = requests.get(link, stream=True)
+        total_length = response.headers.get('content-length')
 
-for x in range(1, 24):
-    if x < 10:
-        print '0' + str(x)
-    else:
-        print str(x)
-
+        if total_length is None: # no content length header
+            f.write(response.content)
+        else:
+            dl = 0
+            total_length = int(total_length)
+            for data in response.iter_content(chunk_size=4096):
+                dl += len(data)
+                f.write(data)
+                done = int(100 * dl / total_length)
+                print done
