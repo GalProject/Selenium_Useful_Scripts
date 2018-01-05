fe8828a61ae2918117fde71bb8e67221523fa5ce
GalBenEvgi <galevgi@gmail.com>
2018-01-05 22:52:37 +0200

    downloadBegova

--- downloadBegova.py
+++ downloadBegova.py
@@ -1,9 +1,7 @@
 import requests
 
-link = 'http://online.flipbuilder.com/aimn/niys/files/mobile/1.jpg'
-
 for x in range(1,89):
-    link = 'http://online.flipbuilder.com/aimn/niys/files/mobile/' + str(x) + '.jpg'
+    link = 'http://online.flipbuilder.com/aimn/jmbg/files/mobile/' + str(x) + '.jpg'
     print link
     f = open(str(x) + '.jpg', 'wb')
     f.write(requests.get(link).content)
