ecbf137b874b0727e3730abff1dc7584c24ef10e
GalBenEvgi <galevgi@gmail.com>
2018-02-25 19:20:25 +0200

    req.txt file added

--- downloadBegova.py
+++ downloadBegova.py
@@ -1,7 +1,8 @@
 import requests
 
+
 for x in range(1,89):
-    link = 'http://online.flipbuilder.com/aimn/jmbg/files/mobile/' + str(x) + '.jpg'
+    link = 'http://online.flipbuilder.com/aimn/aclh/files/mobile/' + str(x) + '.jpg'
     print link
     f = open(str(x) + '.jpg', 'wb')
     f.write(requests.get(link).content)
